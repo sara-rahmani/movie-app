57aa33446893922c536e4c1a8161822ea62f3bba
sara rahmani <sararahmani96@gmail.com>
2023-01-07 17:13:21 -0800

    modified.js filter and home.js to filter the movies

--- movie-app/src/components/Filter.js
+++ movie-app/src/components/Filter.js
@@ -1,24 +1,34 @@
-const Filter = () => {
 
+const Filter = ({  activeList, setActiveList }) => {
+    
+
+  
 return (
     <div className="filter-container">
-      <button
-        
+      <button          onClick={() => setActiveList('popular')}
+
+        className={activeList === "popular" ? "active" : ""}
       >
         Popular
       </button>
       <button
-        
+             onClick={() => setActiveList('top_rated')}
+             className={activeList === "top_rated" ? "active" : ""}
+
+
       >
         Top Rated
       </button>
       <button
-       
+        onClick={() => setActiveList('now_playing')}
+        className={activeList === "now_playing" ? "active" : ""}
+
       >
         Now Playing
       </button>
       <button
-       
+          onClick={() => setActiveList('upcoming')}
+              className={activeList === "upcoming" ? "active" : ""}
       >
 Upcoming      </button>
       
--- movie-app/src/pages/Home.js
+++ movie-app/src/pages/Home.js
@@ -1,10 +1,9 @@
 // Page Home
-
+import { useCallback } from 'react';
 import { useEffect,useState } from "react";
 import Movie from "../components/Movie";
 import Filter from "../components/Filter";
 
-import { appTitle } from "../globals/globalVariables";
 import isFav from "../utilities/isFav";
 import toWatch from "../utilities/toWatch";
 
@@ -14,28 +13,43 @@ function Home() {
     const favs = useSelector((state) => state.favs.items);
     const toWatchs = useSelector((state) => state.laters.items);
 
-  const url =
-    "https://api.themoviedb.org/3/movie/popular?api_key=7670191addb965f127d1f008467d109e&language=en-US&page=1";
-const [popular, setPopular] = useState([]);
-useEffect(() => {
-    fetchPopular();
-  }, []);
-const fetchPopular = async () => {
-    const data = await fetch(url);
-    const movies = await data.json();
-    console.log(movies);
-    setPopular(movies.results.slice(0, 12));
-  };
-  useEffect(() => {
-    document.title = `${appTitle} - Home`;
-  }, []);
+   //const [url,setUrl]=useState("https://api.themoviedb.org/3/movie/popular?api_key=7670191addb965f127d1f008467d109e&language=en-US&page=1");
+const [movies, setMovies] = useState([]);
+const [activeList,setActiveList]=useState('popular');
+const url =
+     `https://api.themoviedb.org/3/movie/${activeList}?api_key=7670191addb965f127d1f008467d109e&language=en-US&page=1`;
+
+     const fetchPopular = useCallback(async () => {
+      console.log(url);
+        const data = await fetch(url);
+        const movies = await data.json();
+        console.log(movies);
+        setMovies(movies.results.slice(0, 12));
+    }, [url])
+    
+    useEffect(() => {
+        fetchPopular();
+      }, [fetchPopular]);
+
+
+
+// const fetchPopular = async () => {
+//   console.log(url);
+//     const data = await fetch(url);
+//     const movies = await data.json();
+//     console.log(movies);
+//     setMovies(movies.results.slice(0, 12));
+//   };
+//   useEffect(() => {
+//     fetchPopular();
+//   }, [activeList,fetchPopular]);
 
   return (
     <main>
       <section>
-      <Filter  />
+      <Filter  activeList={activeList} setActiveList={setActiveList} />
       <div className="movies">
-        {popular.map((movie,i) => {
+        {movies.map((movie,i) => {
           return <Movie
            key={i}
            movie={movie}
