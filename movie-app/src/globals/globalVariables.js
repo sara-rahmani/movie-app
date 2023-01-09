594231344aabc81632a6d6facf4755db6e0f52b7
sara rahmani <sararahmani96@gmail.com>
2023-01-08 22:23:52 -0800

    added lastName of teams memeber to the footer

--- movie-app/src/globals/globalVariables.js
+++ movie-app/src/globals/globalVariables.js
@@ -1,4 +1,4 @@
 export const appTitle = "The Movie App";
-export const appAuthor = "Sara Rahmani & Kriti";
+export const appAuthor = "Sara Rahmani & Kriti Kriti";
 export const favs = "movie-app-favs";
 export const toWatchs = "movie-app-toWatchs";
